e5bd7ac716b87bacf64b4fc49d1f9eeb55c14f49
bitcooker <robertmercado0407@gmail.com>
2024-01-05 15:52:46 +0530

    Refactor call-of-action-card component

--- src/components/landing-page/call-of-action-card.tsx
+++ src/components/landing-page/call-of-action-card.tsx
@@ -1,11 +1,4 @@
-import {
-  Button,
-  Card,
-  Container,
-  Grid,
-  Group,
-  Title,
-} from "@mantine/core";
+import { Button, Card, Container, Grid, Group, Title } from "@mantine/core";
 import { useMediaQuery } from "@mantine/hooks";
 
 export function LandingCOACard() {
@@ -16,13 +9,13 @@ export function LandingCOACard() {
       <Card mx="xl" radius="lg" p="xl">
         <Container size="xl">
           <Grid columns={10}>
-            <Grid.Col span={7}>
+            <Grid.Col span={{ base: 10, md: 7 }}>
               <Title order={2} ta={BiggerThan431 ? "left" : "center"}>
                 Sign up today and unlock the power of esports data!
               </Title>
             </Grid.Col>
 
-            <Grid.Col span={3}>
+            <Grid.Col span={{ base: 10, md: 3 }}>
               <Group justify="center" align="center" h="100%">
                 <Button radius="xl" size="md">
                   Join Now!
