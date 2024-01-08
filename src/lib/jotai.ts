d23a915c11d4e412ef16e808c97ca8ca2692d625
bitcooker <robertmercado0407@gmail.com>
2024-01-08 22:08:07 +0530

    match-page fix

--- src/lib/jotai.ts
+++
@@ -1,5 +0,0 @@
-import { atom } from "jotai";
-import { GAME_LIST , TAB_LIST} from "./data";
-
-export const SelectedGameAtom = atom(GAME_LIST[0]!);
-export const SelectedCompTabAtom=atom(TAB_LIST[0]!)
