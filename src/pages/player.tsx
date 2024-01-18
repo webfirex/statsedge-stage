df95332051b740e60a43c3650e20afefddb9e7c1
bitcooker <robertmercado0407@gmail.com>
2024-01-18 20:19:41 +0530

    Player Page Error Fix

--- src/pages/player.tsx
+++ src/pages/player.tsx
@@ -31,54 +31,14 @@ import { RoleRow } from "~/components/player-page/role-row";
 import MainFilters from "~/components/player-page/main-filter";
 import { BarChart, LineChart } from '@mantine/charts';
 import { data } from '~/pages/api/charts/vbar';
-import { elements } from "./api/charts/match-history";
 import { performance } from "./api/charts/perform-chart";
 import OverallStats from "~/components/player-page/overall-stats";
 import MatchStats from "~/components/player-page/match-stats";
+import PlayerMatchHistory from "~/components/player-page/match-history";
 
 export default function () {
   const BigThenMd = useMediaQuery(`(min-width: ${BREAKPOINTS.MD})`)
-  const BigThenXs = useMediaQuery(`(min-width: ${BREAKPOINTS.XS})`);
-
-  const rows = elements.map((element) => (
-    <Table.Tr key={element.date}>
-      <Table.Td>{element.date}</Table.Td>
-      <Table.Td display={BigThenMd ? "block" : "none"}>
-        <Flex>
-        <Image
-          src={"https://assets-global.website-files.com/622606ef3eafab51dbfa178d/6238793e742015185a0d4095_Gold.svg"}
-          alt="league logo"
-          fit="contain"
-          h={BigThenXs ? 20 : 15}
-        />{element.team}
-        </Flex>
-      </Table.Td>
-      <Table.Td>
-        <Flex display={BigThenMd ? "flex" : "none"}>
-          <Image
-            src={"https://assets-global.website-files.com/622606ef3eafab51dbfa178d/6238793e742015185a0d4095_Gold.svg"}
-            alt="league logo"
-            fit="contain"
-            h={BigThenXs ? 20 : 15}
-          />{element.opponent}
-        </Flex>
-        <Flex display={BigThenMd ? "none" : "flex"}>
-          <Image
-            src={"https://assets-global.website-files.com/622606ef3eafab51dbfa178d/6238793e742015185a0d4095_Gold.svg"}
-            alt="league logo"
-            fit="contain"
-            h={BigThenXs ? 20 : 15}
-          />
-        </Flex>
-      </Table.Td>
-      <Table.Td>{element.map}</Table.Td>
-      <Table.Td>{element.kills}</Table.Td>
-      <Table.Td>{element.headshots}</Table.Td>
-      <Table.Td>{element.kills}</Table.Td>
-      <Table.Td>{element.assists}</Table.Td>
-    </Table.Tr>
-  ));
-
+  
     const sport = SportInfo("lol");
 
     if (!sport) {
@@ -174,6 +134,21 @@ export default function () {
 
             <Space />
 
+            {/* <Grid columns={10}>
+              <Grid.Col span={{ base: 10, md: 7 }}>
+                <Card p={"20px"} style={{ backgroundImage: "url(/map-blur.png)" }}>
+                  <Flex justify={"space-between"}>
+                    <Box style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
+
+                    </Box>
+                  </Flex>
+                </Card>
+              </Grid.Col>
+              <Grid.Col span={{ base: 10, md: 3 }}></Grid.Col>
+            </Grid> */}
+
+            <Space />
+
             <Grid columns={10} mt={"md"}>
               <Grid.Col span={{ base: 10, md: 5 }}>
                 <FadeUpAni>
@@ -203,76 +178,7 @@ export default function () {
             </Grid>
 
             <FadeUpAni>
-              <Card display={"flex"} flex={"column"} style={{ gap: "20px" }}>  
-                <Flex gap={"lg"} wrap={"wrap"} align={"center"} pl={"20px"}>
-                  <Title order={4}>MATCH HISTORY</Title>
-                  <Flex gap={"xs"} align={"center"} display={BigThenMd ? "flex" : "none"}>
-                    <Flex gap={"xs"} align={"baseline"}>
-                      <Text fz={"md"}>
-                        Rows
-                      </Text>
-                      <Select
-                        mt="md"
-                        comboboxProps={{ withinPortal: true }}
-                        data={['React', 'Angular', 'Svelte', 'Vue']}
-                        placeholder="Rows"
-                        radius={"lg"}
-                        // classNames={classes}
-                      />
-                    </Flex>
-                    <Flex gap={"xs"} align={"baseline"}>
-                      <Text fz={"md"}>
-                        Maps
-                      </Text>
-                      <Select
-                        mt="md"
-                        comboboxProps={{ withinPortal: true }}
-                        data={['React', 'Angular', 'Svelte', 'Vue']}
-                        placeholder="All Maps"
-                        radius={"lg"}
-                        // classNames={classes}
-                      />
-                    </Flex>
-                  </Flex>
-                </Flex>
-                
-                <Table.ScrollContainer minWidth={500} bg={"#1d1d1d"} style={{ borderRadius: "15px", display: BigThenMd ? "block" : "none" }}>
-                  <Table striped highlightOnHover  horizontalSpacing="sm" verticalSpacing="sm">
-                    <Table.Thead>
-                      <Table.Tr bg={"#101010"} color="#005BBF">
-                        <Table.Th color="#005BBF">Date</Table.Th>
-                        <Table.Th color="#005BBF" display={BigThenMd ? "block" : "none"}>Player Team</Table.Th>
-                        <Table.Th color="#005BBF">Opponent</Table.Th>
-                        <Table.Th color="#005BBF">Map</Table.Th>
-                        <Table.Th color="#005BBF">Kills</Table.Th>
-                        <Table.Th color="#005BBF">Headshots</Table.Th>
-                        <Table.Th color="#005BBF">Assists</Table.Th>
-                        <Table.Th color="#005BBF">Deaths</Table.Th>
-                      </Table.Tr>
-                    </Table.Thead>
-                    <Table.Tbody>{rows}</Table.Tbody>
-                  </Table>
-                </Table.ScrollContainer>
-                
-                <Table.ScrollContainer minWidth={400} bg={"#1d1d1d"} style={{ borderRadius: "15px", display: BigThenMd ? "none" : "block" }}>
-                  <Table striped highlightOnHover  horizontalSpacing="sm" verticalSpacing="sm">
-                    <Table.Thead>
-                      <Table.Tr bg={"#101010"} color="#005BBF">
-                        <Table.Th color="#005BBF">Date</Table.Th>
-                        <Table.Th color="#005BBF" display={BigThenMd ? "block" : "none"}>Player Team</Table.Th>
-                        <Table.Th color="#005BBF">Opp.</Table.Th>
-                        <Table.Th color="#005BBF">Map</Table.Th>
-                        <Table.Th color="#005BBF">K</Table.Th>
-                        <Table.Th color="#005BBF">HS</Table.Th>
-                        <Table.Th color="#005BBF">A</Table.Th>
-                        <Table.Th color="#005BBF">D</Table.Th>
-                      </Table.Tr>
-                    </Table.Thead>
-                    <Table.Tbody>{rows}</Table.Tbody>
-                  </Table>
-                </Table.ScrollContainer>
-
-              </Card>
+              <PlayerMatchHistory />
             </FadeUpAni>
 
           </Stack>
